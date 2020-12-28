0ff9ef2a907053347a7dce7f88ae4e5098ec7ab5
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-12-28 12:20:04 +0100

    Initial commit

---
+++ src/main/scala/app/SimpleApp.scala
@@ -0,0 +1,19 @@
+package app
+
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "xxx.txt" // Should be some file on your system
+    val spark = SparkSession.builder
+      .appName("Simple Application")
+      .config("spark.master", "local")
+      .getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    println(s"text: $logData")
+    spark.stop()
+  }
+}

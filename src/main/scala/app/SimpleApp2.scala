347d6d3b8e5ed0cf8dfeda0a768bc03424c6e1a5
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-12-28 15:56:45 +0100

    read json with schema

--- src/main/scala/app/SimpleApp2.scala
+++ src/main/scala/app/SimpleApp2.scala
@@ -1,21 +1,29 @@
 package app
 
-import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.spark.sql.functions.{col, explode}
+import org.apache.spark.sql.types.{ArrayType, DataType, IntegerType, StringType, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object SimpleApp2 {
   def main(args: Array[String]) {
     val spark = bootstrapSpark()
-    val schema = new StructType()
-      .add("RecordNumber", IntegerType, nullable = true)
-      .add("Zipcode", IntegerType, nullable = true)
-      .add("ZipCodeType", StringType, nullable = true)
-      .add("City", StringType, nullable = true)
-      .add("State", StringType, nullable = true)
+    val schema = "string STRING, int INT, array ARRAY<INT>, dict STRUCT<key : STRING>"
+    val schemaTyped = new StructType()
+      .add("string", StringType)
+      .add("int", IntegerType)
+      .add("array", ArrayType(StringType))
+      .add("dict", new StructType().add("key", StringType))
+    val source = scala.io.Source.fromFile("schema.json")
+    val lines = try source.getLines.mkString finally source.close()
+    val schemaFromJson = DataType.fromJson(lines).asInstanceOf[StructType]
     val logFile = "Task2_xxx"
-    val logData: DataFrame = spark.read.json(logFile)
+    var logData: DataFrame = spark.read.schema(schemaFromJson).json(logFile)
+    logData = logData
+      .withColumn("xyz", col("dict.key2"))
+      .drop(col("dict"))
 
     logData.show()
+    logData.printSchema()
 
     spark.stop()
   }

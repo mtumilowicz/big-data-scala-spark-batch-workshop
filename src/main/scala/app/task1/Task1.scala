e87d58dc87b65b96abf6436ad8944dcb29ce6515
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-12-31 18:26:04 +0100

    Task1 extends App

--- src/main/scala/app/task1/Task1.scala
+++ src/main/scala/app/task1/Task1.scala
@@ -2,18 +2,16 @@ package app.task1
 
 import org.apache.spark.sql.{Dataset, SparkSession}
 
-object Task1 {
+object Task1 extends App {
 
-  def main(args: Array[String]) {
-    implicit val spark: SparkSession = bootstrapSpark()
-    val file = loadFile("Task1.txt").cache()
-    val fileAnalysis = new TextFileAnalysis(file)
-    val numAs = fileAnalysis.lineContaining("a").count()
-    val numBs = fileAnalysis.lineContaining("b").count()
-    val numCs = fileAnalysis.lineContaining("c").count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs, Lines with b: $numCs")
-    spark.stop()
-  }
+  implicit val spark: SparkSession = bootstrapSpark()
+  val file = loadFile("Task1.txt").cache()
+  val fileAnalysis = new TextFileAnalysis(file)
+  val numAs = fileAnalysis.lineContaining("a").count()
+  val numBs = fileAnalysis.lineContaining("b").count()
+  val numCs = fileAnalysis.lineContaining("c").count()
+  println(s"Lines with a: $numAs, Lines with b: $numBs, Lines with b: $numCs")
+  spark.stop()
 
   def loadFile(filePath: String)(implicit spark: SparkSession): Dataset[String] =
     spark.read.textFile(filePath)

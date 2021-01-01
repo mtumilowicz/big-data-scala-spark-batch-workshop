b2b20b07ee263eee23caa7277059c69c23743231
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-01-01 10:45:51 +0100

    Task4Workshop

---
+++ src/main/scala/app/task4/Task4Workshop.scala
@@ -0,0 +1,20 @@
+package app.task4
+
+import app.task0.SparkWrapper
+import org.apache.spark.sql.DataFrame
+
+object Task4Workshop extends App with SparkWrapper {
+
+  val address: DataFrame = loadJsonFile("task4/Dataset2")
+
+  val customerInfo: DataFrame = loadCsvFile("task4/Dataset1.csv")
+
+  val enrichedAddress = enrich(address, customerInfo)
+
+  investigate(enrichedAddress)
+
+  spark.stop()
+
+  def enrich(data: DataFrame, additionalInfo: DataFrame): DataFrame =
+    data.join(additionalInfo, "CustomerId")
+}
